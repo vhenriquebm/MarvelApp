2603a1433cb51c6654f16e825e08ec1fbf2f200f
vhenriquebm <vhenriquebm@gmail.com>
2024-05-04 15:01:53 -0300

    Navigation flow configured

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.marvel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.marvel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView(binding)
+    }
+
+    private fun initView(binding: ActivityMainBinding) {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener { }
+        }
     }
 }
 
