8582f56471be10f9bfe12287793ef140bed5f3cd
vhenriquebm <vhenriquebm@gmail.com>
2024-05-04 12:17:50 -0300

    Project created

---
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.marvel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marvel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
+
+
